bd8d3f5e6a30f29024aa77a69c39202f2c76fb00
Abhishek K M <67158080+Sync271@users.noreply.github.com>
2022-05-01 19:43:20 +0530

    initialize nextjs

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+	return (
+		<>
+			<Head>
+				<title>Wordle Copilot</title>
+			</Head>
+			<div className="container">
+				<div className="title">Wordle Copilot</div>
+			</div>
+		</>
+	);
+};
+
+export default Home;
